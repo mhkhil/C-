2ffc6b124fb9f904d8b2e6685eb882748c16079c
mhkhil <mhkhilrr@gmail.com>
2024-11-06 21:45:02 +0900

    (2024.11.06) 컨테이너 어댑터

---
+++ Class13th (Linear Container)/Class13th (Linear Container).cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+#pragma region Vector 컨테이너
+
+	// std::vector<int> vector;
+	// 
+	// vector.reserve(6);
+	// 
+	// vector.push_back(10); // [10]
+	// vector.push_back(20); // [10][20]
+	// vector.push_back(30);
+	// vector.push_back(40);
+	// vector.push_back(50);
+	// 
+	// vector.pop_back();
+	// 
+	// vector.push_back(50);
+	// vector.push_back(60);
+	// vector.push_back(70);
+	// 
+	// cout << "Capacity : " << vector.capacity() << endl;
+	// cout << "size : " << vector.size() << endl;
+	// 
+	// for (int i = 0; i < vector.size(); i++)
+	// {
+	// 	cout << vector[i] << endl;
+	// }
+
+#pragma endregion
+
+#pragma region List 컨테이너
+
+	// std::list<int> list;
+	// 
+	// list.push_back(10);
+	// list.push_back(20);
+	// list.push_front(0);
+	// 
+	// list.pop_front();
+	// 
+	// cout << "List의 크기 : " << list.size() << endl;
+	// 
+	// cout << list.front() << endl;
+	// 
+	// cout << list.back() << endl;
+
+#pragma endregion
+
+#pragma region String 컨테이너
+
+	// string name = "Road";
+	// 
+	// name = name + "FC";
+	// 
+	// cout << "name 변수의 값 : " << name << endl;
+	// cout << "name의 capacity 값 : " << name.capacity() << endl;
+	// name.append(" Go For it");
+	// 
+	// cout << "name 변수의 Length : " << name.length() << endl;
+	// 
+	// cout << "name 변수의 값 : " << name << endl;
+	// 
+	// 
+	// for (int i = 0; i < name.size(); i ++)
+	// {
+	// 	if (i != 0 && i % 5 == 0)
+	// 	{
+	// 		name.insert(i, "X");
+	// 	}
+	// }
+	// cout << name << endl;
+	// 
+	// cout << name.compare("for") << endl;
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,43 +1,64 @@
 ﻿#include <iostream>
-#include <vector>
-#include <list>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
 int main()
 {
-#pragma region Vector 컨테이너
-	
-	// std::vector<int> vector;
+#pragma region 컨테이너 어댑터
+	// 기존의 컨테이너의 인터페이스를 제한하여 만든 기능이
+	// 제한되거나 변형되어 있는 컨테이너입니다.
+#pragma region Stack
+
+	// std::stack<int> stack;
 	// 
-	// vector.reserve(6);
+	// stack.push(10); // [10]
+	// stack.push(20); // [20] [10]
+	// stack.push(30); // [30] [20] [10]
+	// stack.push(40); // [40] [30] [20] [10]
 	// 
-	// vector.push_back(10); // [10]
-	// vector.push_back(20); // [10][20]
-	// vector.push_back(30);
-	// vector.push_back(40);
-	// vector.push_back(50);
+	// stack.pop();    // [30] [20] [10]
 	// 
-	// vector.pop_back();
+	// cout << "Stack의 top의 값 : " << stack.top() << endl;
+	// cout << "Stack의 size의 값 : " << stack.size() << endl;
+	// cout << "Stack의 empty의 값 : " << stack.empty() << endl;
 	// 
-	// vector.push_back(50);
-	// vector.push_back(60);
-	// vector.push_back(70);
+	// int size = stack.size();
 	// 
-	// cout << "Capacity : " << vector.capacity() << endl;
-	// cout << "size : " << vector.size() << endl;
-	// 
-	// for (int i = 0; i < vector.size(); i++)
+	// for (int i = 0; i < size; i++)
 	// {
-	// 	cout << vector[i] << endl;
+	// 	cout << "Stack의 top의 값 : " << stack.top() << endl;
+	// 	stack.pop();
+	// }
+	// 
+	// while (stack.empty() == false)
+	// {
+	// 	cout << stack.top() << endl;
+	// 	stack.pop();
 	// }
 
 #pragma endregion
 
-#pragma region List 컨테이너
+#pragma region Queue
+
+	std::queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+	cout << queue.front() << endl;
+	queue.pop();
+	cout << queue.front() << endl;
+	queue.pop();
+	cout << queue.front() << endl;
+
+#pragma endregion
+
 
 
 #pragma endregion
+
 
 	return 0;
 }
